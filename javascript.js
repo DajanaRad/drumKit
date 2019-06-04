4a34854d577f982a639ee8c6f5afc3a5738bb0f4
DajanaRad <dajanarad@gmail.com>
2019-06-04 09:59:33 +0200

    mobile design changes

--- javascript.js
+++ javascript.js
@@ -1,14 +1,28 @@
-window.addEventListener('keydown', function(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if(!audio) return;
-    audio.play();
-    key.classList.add('playing');
-})
+const keys = document.querySelectorAll(".key");
+console.log(keys);
 function removeTransition(e) {
-    console.log(e);
-    if(e.propertyName === 'transform') return;
-    this.classList.remove('playing');
+  console.log(e);
+  if (e.propertyName === "transform") return;
+  this.classList.remove("playing");
 }
-const keys = document.querySelectorAll('.key');
-keys.forEach((key) =>key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener("keydown", function(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.play();
+  key.classList.add("playing");
+});
+
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
+for (var i = 0; i < keys.length; i++) {
+  const audioClick = document.querySelectorAll("audio");
+  console.log(audioClick);
+  (function(index) {
+    keys[index].addEventListener("click", function() {
+      audioClick[index].play();
+      this.classList.add("playing");
+    });
+  })(i);
+}
